b3cb78a9d9c6c32cadd0e6cc35cac4196bdc451b
Matthew Hartley <mhartley@cantab.net>
2013-12-19 12:12:46 +0000

    Added README, got far enough into code to require libpng

---
+++ README.md
@@ -0,0 +1,3 @@
+# FastImgDict - fast image dictionary
+
+Load a PNG image into a python dictionary. The pixel RGB values in each cell of the image represent an identifier. The dictionary uses these identifiers as keys and a list of all x, y coordinates that represent that identifier as values.
--- src/fastimgdict.c
+++ src/fastimgdict.c
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[])
 {
   char sig[8];
+  unsigned long width, height;
 
   FILE* fp = fopen("data/T03.png", "rb");
   if (!fp) {
@@ -17,6 +18,12 @@ int main(int argc, char *argv[])
   }
 
   fread(sig, 1, 8, fp);
+  if (!png_check_sig(sig, 8)) {
+    fprintf(stderr, "File is not a valid PNG file\n");
+    exit(2);
+  }
+
+
 
   return 0;
 }
