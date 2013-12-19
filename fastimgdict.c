ee9db7e3c969a8f66a7f59db08cb1bf52edb3ea9
Matthew Hartley <mhartley@cantab.net>
2013-12-19 11:55:33 +0000

    Initial commit

---
+++ fastimgdict.c
@@ -0,0 +1,22 @@
+/**************
+ * FastImgDict
+ **************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<png.h>
+
+int main(int argc, char *argv[])
+{
+  char sig[8];
+
+  FILE* fp = fopen("T03.png", "rb");
+  if (!fp) {
+    fprintf(stderr, "Failed to open image file\n");
+    exit(2);
+  }
+
+  fread(sig, 1, 8, fp);
+
+  return 0;
+}
